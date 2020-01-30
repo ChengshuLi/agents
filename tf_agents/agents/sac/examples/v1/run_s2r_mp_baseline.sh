de76ce21168a66e10f0e770a85594bdf9d21afef
Chengshu Li <chengshu@stanford.edu>
2020-01-29 19:15:18 -0800

    baseline code done, add training bash scripts

---
+++ tf_agents/agents/sac/examples/v1/run_s2r_mp_baseline.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+gpu_c="1"
+gpu_g="0"
+algo="sac"
+robot="fetch"
+config_file="../examples/configs/"$robot"_interactive_nav_s2r_mp_continuous.yaml"
+col="0.0"
+run="0"
+lr="3e-4"
+
+log_dir="test_s2r_candcenter_push_door_baseline_test"
+echo $log_dir
+
+#python -u train_eval.py \
+#    --root_dir $log_dir \
+#    --env_type ig_s2r_mp_push_door_baseline \
+#    --config_file $config_file \
+#    --initial_collect_steps 200 \
+#    --collect_steps_per_iteration 1 \
+#    --batch_size 256 \
+#    --train_steps_per_iteration 1 \
+#    --replay_buffer_capacity 10000 \
+#    --num_eval_episodes 100 \
+#    --eval_interval 10000000 \
+#    --gpu_c $gpu_c \
+#    --gpu_g $gpu_g \
+#    --num_parallel_environments 1 \
+#    --actor_learning_rate $lr \
+#    --critic_learning_rate $lr \
+#    --alpha_learning_rate $lr \
+#    --collision_reward_weight $col \
+#    --eval_only \
+#    --env_mode "gui"
+#exit
+
+nohup python -u train_eval.py \
+    --root_dir $log_dir \
+    --env_type ig_s2r_mp_push_door_baseline \
+    --config_file $config_file \
+    --initial_collect_steps 200 \
+    --collect_steps_per_iteration 1 \
+    --batch_size 256 \
+    --train_steps_per_iteration 1 \
+    --replay_buffer_capacity 10000 \
+    --num_eval_episodes 1 \
+    --eval_interval 10000000 \
+    --gpu_c $gpu_c \
+    --gpu_g $gpu_g \
+    --num_parallel_environments 8 \
+    --actor_learning_rate $lr \
+    --critic_learning_rate $lr \
+    --alpha_learning_rate $lr \
+    --collision_reward_weight $col > $log_dir".log"
