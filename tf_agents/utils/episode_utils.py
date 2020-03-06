9adccc7f30896dc9a44a60d61be18f1806cc6e73
LC1905 <canliu1905@gmail.com>
2020-03-05 16:44:24 -0800

    personal space violation metrics

--- tf_agents/utils/episode_utils.py
+++ tf_agents/utils/episode_utils.py
@@ -8,6 +8,11 @@ import pickle
 
 def get_metrics(episodes):
     success_rate = np.mean([episode.success for episode in episodes])
+    collision_rate = np.mean([episode.collision for episode in episodes])
+    timeout_rate = np.mean([episode.timeout for episode in episodes])
+    personal_space_violation = np.mean([episode.personal_space_violation for episode in episodes])
+    total_personal_space = np.mean([episode.total_personal_space for episode in episodes])
+    min_personal_space = np.mean([episode.min_personal_space for episode in episodes])
     # spl = np.mean([episode.success * episode.path_efficiency for episode in episodes])
     # kinematic_disturbance = np.mean([episode.success * episode.kinematic_disturbance for episode in episodes])
     # dynamic_disturbance_a = np.mean([episode.success * episode.dynamic_disturbance_a for episode in episodes])
@@ -15,6 +20,11 @@ def get_metrics(episodes):
     # collision_step = np.mean([episode.collision_step for episode in episodes])
     return {
         'success_rate': success_rate,
+        'collision_rate': collision_rate,
+        'timeout_rate': timeout_rate,
+        'personal_space_violation': personal_space_violation,
+        'average_personal_space': average_personal_space,
+        'min_personal_space': min_personal_space,
         # 'spl': spl,
         # 'kinematic_disturbance': kinematic_disturbance,
         # 'dynamic_disturbance_a': dynamic_disturbance_a,
