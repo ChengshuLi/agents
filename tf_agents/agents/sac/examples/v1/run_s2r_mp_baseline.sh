b71f05e76000538529e9cb8bb58ee4cf6e0a9be9
fxia22 <xf1280@gmail.com>
2020-07-12 22:13:14 +0000

    baseline ready to train with 9 parallel envs and specified model_ids

--- tf_agents/agents/sac/examples/v1/run_s2r_mp_baseline.sh
+++ tf_agents/agents/sac/examples/v1/run_s2r_mp_baseline.sh
@@ -6,52 +6,34 @@ algo="sac"
 robot="fetch"
 config_file="../examples/configs/"$robot"_interactive_nav_s2r_mp_continuous.yaml"
 col="0.0"
-run="0"
 lr="3e-4"
+task="push_door"
+run="0"
 
-log_dir="/result/flat_baseline_push_door"
+log_dir="/result/flat_rl_baseline_"$task"_"$run
+env_type="ig_s2r_mp_"$task"_baseline"
 echo $log_dir
-
-#python -u train_eval.py \
-#    --root_dir $log_dir \
-#    --env_type ig_s2r_mp_empty_baseline \
-#    --config_file $config_file \
-#    --initial_collect_steps 200 \
-#    --collect_steps_per_iteration 1 \
-#    --batch_size 256 \
-#    --train_steps_per_iteration 1 \
-#    --replay_buffer_capacity 10000 \
-#    --num_eval_episodes 100 \
-#    --eval_interval 10000000 \
-#    --gpu_c $gpu_c \
-#    --gpu_g $gpu_g \
-#    --num_parallel_environments 1 \
-#    --actor_learning_rate $lr \
-#    --critic_learning_rate $lr \
-#    --alpha_learning_rate $lr \
-#    --collision_reward_weight $col \
-#    --eval_only \
-#    --env_mode "gui"
-#exit
+echo $env_type
+mkdir -p $log_dir
 
 nohup python -u train_eval.py \
     --root_dir $log_dir \
-    --env_type ig_s2r_mp_push_door \
+    --env_type $env_type \
     --config_file $config_file \
     --initial_collect_steps 200 \
     --collect_steps_per_iteration 30 \
+    --num_iterations 100000000 \
     --batch_size 256 \
     --train_steps_per_iteration 1 \
     --replay_buffer_capacity 10000 \
     --num_eval_episodes 1 \
-    --eval_interval 10000000 \
+    --eval_interval 100000000 \
     --gpu_c $gpu_c \
     --gpu_g $gpu_g \
-    --num_parallel_environments 1 \
+    --num_parallel_environments 9 \
     --actor_learning_rate $lr \
     --critic_learning_rate $lr \
     --alpha_learning_rate $lr \
-    --collision_reward_weight $col > $log_dir".log"
+    --model_ids Avonia,Avonia,Avonia,candcenter,candcenter,candcenter,gates_jan20,gates_jan20,gates_jan20 \
+    --collision_reward_weight $col > $log_dir/log &
 
-#    --model_ids Avonia,Avonia,Avonia,candcenter,candcenter,candcenter,gates_jan20,gates_jan20 \
-#    --model_ids Avonia,Avonia,Avonia,Avonia,gates_jan20,gates_jan20,gates_jan20,gates_jan20 \
